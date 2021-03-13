cfe5428845dcc98313f71ee02f14a9166e9526e1
Masao Kitamura <masaok@gmail.com>
2021-03-12 19:55:17 -0800

    Removing unused code

--- Collapsible.js
+++ Collapsible.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Animated, Easing } from 'react-native';
-import { ViewPropTypes } from './config';
 
 const ANIMATED_EASING_PREFIXES = ['easeInOut', 'easeOut', 'easeIn'];
 
